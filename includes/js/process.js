f62621c66623b465768854faff118f017e2ff3a1
Levi <levidps@gmail.com>
2018-02-02 10:59:50 +1100

    refactoring FE js for better private logic

--- includes/js/process.js
+++ includes/js/process.js
@@ -1,69 +1,122 @@
-var getDonationAmount = function(){
-	var amount;
-	var input_amount = document.querySelector('input[name="donation_amount"]:checked');
-	if (input_amount !== null) {
+/**
+ * WP Stripe Donations: Stripe Processing
+ * Payment processing for FE
+ *
+ * @version 0.02
+ */
+var donationsComponent = function() {
 
-		if (input_amount.value !== 'other') {
-			amount = input_amount.value * 100;
-		}
+	var s = {};
 
-		if (input_amount.value === 'other') {
-			var amount_other = document.getElementById('otherAmount');
-			amount = amount_other.value * 100;
+	s.ui = {
+		stripeButton: 	document.getElementById('stripeButton'),
+		input_amount: 	document.querySelector('input[name="donation_amount"]:checked'),
+		amount_other: 	document.getElementById('otherAmount'),
+		monthly_label: 	document.querySelector('.donate_monthly label')
+	};
+
+	s.getDonationAmount = function(){
+		var amount = false;
+
+		if ( s.ui.input_amount !== null) {
+
+			if ( s.ui.input_amount.value !== 'other') {
+				amount = s.ui.input_amount.value * 100;
+			}
+
+			if ( s.ui.input_amount.value === 'other') {
+				amount = s.ui.amount_other.value * 100;
+			}
+
 		}
 
 		return amount;
+	};
 
-	} else {
-		return false;
-	}
-};
-
-var init = function(){
-
-	var handler = StripeCheckout.configure({
-		key: stripe_vars.publishable_key,
-		locale: 'auto',
-		token: function(token) {
-			// You can access the token ID with `token.id`.
-			// Get the token ID to your server-side code for use.
-			var stripeForm = document.getElementById('donateStripe');
-			var elToken = document.getElementById('stripeToken');
-			var elAmount = document.getElementById('stripeAmount');
-			var amount = getDonationAmount();
-			elToken.setAttribute('value', token.id);
-			elAmount.setAttribute('value', amount);
-			stripeForm.submit();
+	s.monthlyDonation = function() {
+		var input = document.getElementById('donate_monthly');
+		if ( input.checked === true ) {
+			document.querySelector('input[name="donate_monthly"]').value = 'false';
+		} else {
+			document.querySelector('input[name="donate_monthly"]').value = 'true';
 		}
-	});
+	};
 
-	var stripeButton = document.getElementById('stripeButton');
+	s.init = (function() {
 
-	if (stripeButton) {
-		stripeButton.removeAttribute('disabled', false);
-		stripeButton.addEventListener('click', function(e) {
-			e.preventDefault();
-			var amount = getDonationAmount();
-			var amountDisplay = (amount/100);
-			var description = 'Charitable Donation $' + amountDisplay + ' ' + stripe_vars.currency.toUpperCase();
-			if (amount>50) {
-				// Open Checkout with further options:
-				handler.open({
-					name: 'Rapid Relief Team',
-					description: description,
-					amount: amount,
-					currency: stripe_vars.currency,
-					billingAddress: true,
-					panelLabel: 'Donate'
-				});
+		var handler = StripeCheckout.configure({
+			key: stripe_vars.publishable_key,
+			locale: 'auto',
+			token: function(token) {
+				// You can access the token ID with `token.id`.
+				// Get the token ID to your server-side code for use.
+				var stripeForm = document.getElementById('donateStripe');
+				var elToken = document.getElementById('stripeToken');
+				var elAmount = document.getElementById('stripeAmount');
+				var amount = s.getDonationAmount();
+				elToken.setAttribute('value', token.id);
+				elAmount.setAttribute('value', amount);
+				stripeForm.submit();
 			}
 		});
-	}
 
-	// Close Checkout on page navigation:
-	window.addEventListener('popstate', function() {
-		handler.close();
+		s.ui.monthly_label.addEventListener('click', function() {
+			s.monthlyDonation();
+		});
+
+		if ( s.ui.stripeButton) {
+			s.ui.stripeButton.removeAttribute('disabled', false);
+			s.ui.stripeButton.addEventListener('click', function(e) {
+				e.preventDefault();
+
+
+				// change donation button label for monthly donations
+				var buttonLabel = 'Donate';
+				if ( document.getElementById('donate_monthly').checked === true ) {
+
+					buttonLabel = 'Donate Monthly';
+				}
+
+				var amount = s.getDonationAmount();
+				if (amount>50) {
+					// Open Checkout with further options:
+					handler.open({
+						name: 'Charitable Donation',
+						description: 'The Murle Education Foundation',
+						amount: amount,
+						currency: 'AUD',
+						billingAddress: true,
+						panelLabel: buttonLabel
+					});
+				}
+
+
+				var amount = s.getDonationAmount();
+				var amountDisplay = (amount/100);
+				var description = 'Charitable Donation $' + amountDisplay + ' ' + stripe_vars.currency.toUpperCase();
+				if (amount>50) {
+					// Open Checkout with further options:
+					handler.open({
+						name: 'Rapid Relief Team',
+						description: description,
+						amount: amount,
+						currency: stripe_vars.currency,
+						billingAddress: true,
+						panelLabel: 'Donate'
+					});
+				}
+			});
+		}
+
+		// Close Checkout on page navigation:
+		window.addEventListener('popstate', function() {
+			handler.close();
+		});
 	});
+
+	return s;
 };
 
-window.addEventListener('load', init, false);
+var
+
+window.addEventListener('load', donationsComponent.init(), false);
